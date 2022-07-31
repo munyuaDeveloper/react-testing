36c3597a7ca9f812010de90cc803070d9862bfad
munyuaDeveloper <munyuapeter06@gmail.com>
2022-07-31 13:09:47 +0300

    React testing, login form example

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders the page title', () => {
+//   render(<App />);
+//   // const pageTitle = screen.getByText(/react testing library/i);
+//   const pageTitle = screen.getByTestId('title')
+//   expect(pageTitle).toBeInTheDocument();
+// });
+
+// test('Test if there are 3 list items', () => {
+//   render(<App />);
+//   const list = screen.getAllByRole('listitem')
+//   expect(list).toHaveLength(3)
+// });
+
+// test('sum to be  6', () => {
+//   render(<App />);
+//   const sum = screen.getByTitle('sum')
+//   expect(sum.textContent).toBe("6")
+// });
